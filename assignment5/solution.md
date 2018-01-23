230b159223ce4e7af2b10b2808e99178b7f6a3a2
Clemens Damke <admin@cortys.de>
2018-01-23 12:56:20 +0100

    added assignment 5 task 1

--- assignment5/solution.md
+++ assignment5/solution.md
@@ -3,6 +3,30 @@
 <h5>Clemens Damke - 7011488</h5>
 </center>
 
+<style>
+	li {
+		page-break-inside: avoid;
+	}
+	.img {
+		display: flex;
+		page-break-inside: avoid;
+		page-break-before: avoid;
+		justify-content: center;
+	}
+	.img * {
+		width: 45%;
+		height: 45%;
+	}
+	
+	h1, h2, h4, h5 {
+		page-break-after: avoid;
+	}
+	
+	ul, pre {
+		page-break-before: avoid;
+	}
+</style>
+
 ## 1. Context Free Grammars
 
 #### 1.1. POS-Tags
@@ -11,4 +35,83 @@
 * `what_WDT flights_NNS leave_VBP los_NNS angeles_NNS arriving_VBG in_IN minneapolis_NN ._.`
 * `what_WDT flights_NNS are_VBP available_JJ between_IN chicago_NNP and_CC indianapolis_NNP ._.`
 
-#### 1.2. 
+#### 1.2. Context Free Grammar
+
+```js
+S := (NP VP | VP | WH_NP VP) END
+NP := NOUN | NP GP | NP CC NP
+VP := VERB NP? PP? | BE (GP | AP) | VP TO VP | VP CC VP
+GP := GERUND NP? PP?
+AP := ADJ PP?
+WH_NP := WP NP
+PP := PREP NP
+WP := 'what'
+CC := 'and'
+NOUN := 'flights' | 'boston' | 'pittsburgh' | 'los angeles'
+      | 'minneapolis' | 'chicago' | 'indianapolis'
+VERB := 'leave' | 'pittsburgh' | 'indianapolis'
+GERUND := 'arriving'
+BE := 'are'
+TO := 'to'
+PREP := 'in' | 'to' | 'between'
+ADJ := 'available'
+END := '.' | '?'
+```
+
+#### 1.3. CFG Parse Trees
+
+* `what flights leave boston to pittsburgh .`
+	<div class="img">
+		<img src="img/normal/s1p0.png"><img src="img/normal/s1p1.png">
+	</div>
+* `what flights leave los angeles arriving in minneapolis .`
+	<div class="img">
+		<img src="img/normal/s2p0.png"><img src="img/normal/s2p1.png">
+	</div>
+* `what flights are available between chicago and indianapolis .`
+	<div class="img">
+		<img src="img/normal/s3p0.png"><img src="img/normal/s3p1.png">
+	</div>
+
+#### 1.4. Chomsky Normal Form
+
+##### 1.4.1. CNF Grammar
+```js
+S := X1 END | VP END | VERB END
+X1 := NP VP | NOUN VP | WH_NP VP | NP VERB | NOUN VERB | WH_NP VERB
+NP := NP GP | NP X2 | NOUN GP | NOUN X2 | NP GERUND | NOUN GERUND
+X2 := CC NP | CC NOUN
+VP := VERB NP | VERB NOUN | VERB PP | VERB X3 | BE GP | BE GERUND
+    | BE AP | BE ADJ | VP X4 | VERB X4
+X3 := NP PP | NOUN PP
+X4 := TO VP | TO VERB | CC VP | CC VERB
+GP := GERUND NP | GERUND PP | GERUND X3
+AP := ADJ PP
+WH_NP := WP NP | WP NOUN
+PP := PREP NP | PREP NOUN
+WP := 'what'
+CC := 'and'
+NOUN := 'flights' | 'boston' | 'pittsburgh' | 'los angeles'
+      | 'minneapolis' | 'chicago' | 'indianapolis'
+VERB := 'leave' | 'pittsburgh' | 'indianapolis'
+GERUND := 'arriving'
+BE := 'are'
+TO := 'to'
+PREP := 'in' | 'to' | 'between'
+ADJ := 'available'
+END := '.' | '?'
+```
+
+##### 1.4.2. CNF Parse Trees
+* `what flights leave boston to pittsburgh .`
+	<div class="img">
+		<img src="img/cnf/s1p0.png"><img src="img/cnf/s1p1.png">
+	</div>
+* `what flights leave los angeles arriving in minneapolis .`
+	<div class="img">
+		<img src="img/cnf/s2p0.png"><img src="img/cnf/s2p1.png">
+	</div>
+* `what flights are available between chicago and indianapolis .`
+	<div class="img">
+		<img src="img/cnf/s3p0.png"><img src="img/cnf/s3p1.png">
+	</div>
--- assignment5/src/task1.clj
+++ assignment5/src/task1.clj
@@ -2,9 +2,9 @@
   (:require [instaparse.core :as insta :refer [defparser]]))
 
 (defparser grammar
-  "S := (NP VP | VP | WH_NP VP) <END>
+  "S := (NP VP | VP | WH_NP VP) END
    NP := NOUN | NP GP | NP CC NP
-   VP := VERB NP? PP? | BE (GP | AP)?
+   VP := VERB NP? PP? | BE (GP | AP) | VP TO VP | VP CC VP
    GP := GERUND NP? PP?
    AP := ADJ PP?
    WH_NP := WP NP
@@ -12,23 +12,56 @@
    WP := 'what'
    CC := 'and'
    NOUN := <SP> ('flights' | 'boston' | 'pittsburgh' | 'los angeles' | 'minneapolis' | 'chicago' | 'indianapolis') <SP>
-   VERB := <SP> 'leave' <SP>
+   VERB := <SP> ('leave' | 'pittsburgh' | 'indianapolis') <SP>
    GERUND := <SP> 'arriving' <SP>
    BE := <SP> 'are' <SP>
+   TO := <SP> 'to' <SP>
    PREP := <SP> ('in' | 'to' | 'between') <SP>
    ADJ := <SP> 'available' <SP>
-   END := '.' | '?'
+   END := <SP> ('.' | '?') <SP>
+   SP := ' '*")
+
+(defparser cnf-grammar
+  "S := X1 END | VP END | VERB END
+   X1 := NP VP | NOUN VP | WH_NP VP | NP VERB | NOUN VERB | WH_NP VERB
+   NP := NP GP | NP X2 | NOUN GP | NOUN X2 | NP GERUND | NOUN GERUND
+   X2 := CC NP | CC NOUN
+   VP := VERB NP | VERB NOUN | VERB PP | VERB X3 | BE GP | BE GERUND | BE AP | BE ADJ | VP X4 | VERB X4
+   X3 := NP PP | NOUN PP
+   X4 := TO VP | TO VERB | CC VP | CC VERB
+   GP := GERUND NP | GERUND PP | GERUND X3
+   AP := ADJ PP
+   WH_NP := WP NP | WP NOUN
+   PP := PREP NP | PREP NOUN
+   WP := 'what'
+   CC := 'and'
+   NOUN := <SP> ('flights' | 'boston' | 'pittsburgh' | 'los angeles' | 'minneapolis' | 'chicago' | 'indianapolis') <SP>
+   VERB := <SP> ('leave' | 'pittsburgh' | 'indianapolis') <SP>
+   GERUND := <SP> 'arriving' <SP>
+   BE := <SP> 'are' <SP>
+   TO := <SP> 'to' <SP>
+   PREP := <SP> ('in' | 'to' | 'between') <SP>
+   ADJ := <SP> 'available' <SP>
+   END := <SP> ('.' | '?') <SP>
    SP := ' '*")
 
 (def parses {:s1 (insta/parses grammar "what flights leave boston to pittsburgh .")
              :s2 (insta/parses grammar "what flights leave los angeles arriving in minneapolis .")
              :s3 (insta/parses grammar "what flights are available between chicago and indianapolis .")})
 
+(def cnf-parses {:s1 (insta/parses cnf-grammar "what flights leave boston to pittsburgh .")
+                 :s2 (insta/parses cnf-grammar "what flights leave los angeles arriving in minneapolis .")
+                 :s3 (insta/parses cnf-grammar "what flights are available between chicago and indianapolis .")})
+
 (defn visualize-parses
   [parses prefix]
   (doseq [[i parse] (map-indexed vector parses)]
-    (insta/visualize parse :output-file (str prefix "p" i ".png"))))
+    (insta/visualize parse :output-file (str prefix "p" i ".png") :output-format "png")))
 
-(visualize-parses (:s1 parses) "img/s1")
-(visualize-parses (:s2 parses) "img/s2")
-(visualize-parses (:s3 parses) "img/s3")
+(visualize-parses (:s1 parses) "img/normal/s1")
+(visualize-parses (:s2 parses) "img/normal/s2")
+(visualize-parses (:s3 parses) "img/normal/s3")
+
+(visualize-parses (:s1 cnf-parses) "img/cnf/s1")
+(visualize-parses (:s2 cnf-parses) "img/cnf/s2")
+(visualize-parses (:s3 cnf-parses) "img/cnf/s3")
